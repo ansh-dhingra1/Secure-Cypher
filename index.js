a6dab1279019e148102fa5b2e72e9d5606d1e021
anshd7655 <92250040+anshd7655@users.noreply.github.com>
2024-04-19 11:38:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+
+submitBtn.addEventListener("click", () => {
+    const val =userName.value;
+    if (val.trim() !== "" && userName.checkValidity()) {
+        // console.log(val);
+        generatePDF(val);
+      } else {
+        userName.reportValidity();
+      }
+});
+const generatePDF = async (name) => {
+    const existingPdfBytes = await fetch("Certificate_RedTeaming.pdf").then((res) =>
+      res.arrayBuffer()
+    );
+
+    // Load a PDFDocument from the existing PDF bytes
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.registerFontkit(fontkit);
+
+    
+  //get font
+  const fontBytes = await fetch("Paul-le1V.ttf").then((res) =>
+  res.arrayBuffer()
+);
+  // Embed our custom font in the document
+  const SanChezFont  = await pdfDoc.embedFont(fontBytes);
+   // Get the first page of the document
+   const pages = pdfDoc.getPages();
+   const firstPage = pages[0];
+ 
+   // Draw a string of text diagonally across the first page
+   firstPage.drawText(name, {
+     x: 600,
+     y: 470,
+     size: 55,
+     font: SanChezFont ,
+     color: rgb(1.0, 0.84, 0.00),
+   });
+ 
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+
+  saveAs(pdfDataUri,"Certificate for Completion.pdf")
+};
+
+ 
